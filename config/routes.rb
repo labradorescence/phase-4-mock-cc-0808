507d851747bd15473a4fc3761c56ef1ea5035501
Shadiya Egal <shadiya325@gmail.com>
2023-03-01 12:04:14 -0500

    practice cc

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :signups, only: [:index, :create, :show]
+  resources :activities, only: [:index, :destroy]
+  resources :campers, only: [:index, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
